47a686d0f44c88a9c0e8d0bfc8ec40f5de6d7bbf
Jakob Meier <comcloudway@ccw.icu>
2020-05-18 18:03:36 +0200

    Fixed Web

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
+const fs = require("fs")
 const app = express();
+//app.use('/',express.static(path.join(__dirname,'build','web')))
+app.use('/',serveStatic(path.join(__dirname,'build','web')));
 
-app.use(serveStatic(path.join(__dirname,'build','web')));
 app.listen(3000)
